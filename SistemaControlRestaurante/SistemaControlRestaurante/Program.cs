5c976311f1961611656cda5748d6c63d6cb6f971
LizzieCernaa <98780118+LizzieCernaa@users.noreply.github.com>
2022-05-16 14:16:26 -0600

    creacion de proyecto

---
+++ SistemaControlRestaurante/SistemaControlRestaurante/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SistemaControlRestaurante
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

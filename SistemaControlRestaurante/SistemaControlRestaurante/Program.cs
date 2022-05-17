7d6c9f653892c49f633c169a3b2d09a0b0345a80
LizzieCernaa <98780118+LizzieCernaa@users.noreply.github.com>
2022-05-17 14:58:07 -0600

    FILE MENU

--- SistemaControlRestaurante/SistemaControlRestaurante/Program.cs
+++ SistemaControlRestaurante/SistemaControlRestaurante/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace SistemaControlRestaurante
 {
@@ -6,10 +7,43 @@ namespace SistemaControlRestaurante
     {
         static void Main(string[] args)
         {
+            
+
+            imprimirMenu();
+            int menu = int.Parse(Console.ReadLine());
+
+            if (menu == 1) 
+            {
+                menuDia();
+            }
+            
+
+
+            Console.ReadKey();
+        }
+
+        static void imprimirMenu()
+        {
+
+            Console.WriteLine("---- MENU ----");
+            Console.WriteLine("1. Mostrar menu del dia");
+            Console.WriteLine("10. SALIR");
+
+        }
+        static void menuDia() 
+        {
             DateTime hoy = DateTime.Now;
             Console.WriteLine((int)hoy.DayOfWeek);
-            Console.ReadKey();
+           /// Console.ReadKey();
+           
+            Console.WriteLine("El menu de este dia es: ");
+            Console.ReadLine();
+            int menu;
 
+            using (var fileSream = new FileStream("Menu.cvs", FileMode.Append)) 
+            {
+                
+            }
         }
     }
 }

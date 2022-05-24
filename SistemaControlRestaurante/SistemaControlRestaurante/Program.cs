30263925061cac89daf8fea136d88f3b11bf3a50
LizzieCernaa <98780118+LizzieCernaa@users.noreply.github.com>
2022-05-24 15:41:13 -0600

    opciones de menus

--- SistemaControlRestaurante/SistemaControlRestaurante/Program.cs
+++ SistemaControlRestaurante/SistemaControlRestaurante/Program.cs
@@ -5,6 +5,7 @@ namespace SistemaControlRestaurante
 {
     internal class Program
     {
+        static int tableWidth = 100;
         static void Main(string[] args)
         {
             int opcion = 0;
@@ -21,16 +22,22 @@ namespace SistemaControlRestaurante
                     int dayOfWeek = (int)DateTime.Now.DayOfWeek;
                     ImprimirMenuDia(dayOfWeek);
                 }
-                else if (opcion == 2) 
+                else if (opcion == 2)
                 {
                     MenuControlOrden();
                     Console.WriteLine("Seleccione una opcion");
                     opcionControlOrden = int.Parse(Console.ReadLine());
-                    if (opcionControlOrden == 1) 
+                    if (opcionControlOrden == 1)
                     {
-                       mostrarOrdenes();
+                        mostrarOrdenes(string.Empty);
                         ordenes = int.Parse(Console.ReadLine());
 
+                    }
+                    else if (opcionControlOrden == 2) 
+                    {
+                        Console.WriteLine("Intruduce una fecha");
+                        string fecha = Console.ReadLine();
+                        mostrarOrdenes(fecha);
                     }
                     Console.ReadKey();
                 }
@@ -38,7 +45,7 @@ namespace SistemaControlRestaurante
             //Console.ReadKey();
         }
 
-        static void ImprimirMenuDia( int dayOfWeek) 
+        static void ImprimirMenuDia(int dayOfWeek)
         {
             string ruta = "menu.csv";
             StreamReader lector = new StreamReader(ruta);
@@ -47,21 +54,21 @@ namespace SistemaControlRestaurante
             string linea = string.Empty;
 
             Console.Clear();
-            Console.WriteLine($"********** MENU DEL DIA **********");
-
-            while ((linea = lector.ReadLine()) != null) 
+            PrintLine();
+            PrintRow($"********** MENU DEL DIA **********");
+            PrintLine();
+            while ((linea = lector.ReadLine()) != null)
             {
                 plato = linea.Split(',');
-                 
-                if (plato[4].Equals ("dia") || dayOfWeek == int.Parse(plato[4])) 
+
+                if (plato[4].Equals("dia") || dayOfWeek == int.Parse(plato[4]))
                 {
-                    Console.WriteLine($"{plato[1]}    {plato[2]}     {plato[3]} ");
-                    Console.WriteLine($"--------------------------------------------------------");
+                    PrintRow(plato[1], plato[2], plato[3]);
+                    PrintLine(); 
 
                 }
             }
 
-            Console.WriteLine($" ************************************************ ");
             lector.Close();
             Console.ReadKey();
         }
@@ -84,7 +91,7 @@ namespace SistemaControlRestaurante
             Console.WriteLine("3. Salir");
         }
 
-        static void mostrarOrdenes()
+        static void mostrarOrdenes( string fecha)
         {
             string ruta = "Ordenes.csv";
             StreamReader lector = new StreamReader(ruta);
@@ -93,20 +100,55 @@ namespace SistemaControlRestaurante
             string linea = string.Empty;
 
             Console.Clear();
-            Console.WriteLine($"********** MENU DEL DIA **********");
-
+            PrintLine();
+            PrintRow("********** MENU DEL DIA **********");
+            PrintLine();
             while ((linea = lector.ReadLine()) != null)
             {
                 orden = linea.Split(',');
-
-                    Console.WriteLine($"{orden[0].ToUpper()}              {orden[1].ToUpper()}             {orden[2].ToUpper()}         {orden[3].ToUpper()}       {orden[4].ToUpper()}        {orden[5].ToUpper()}        {orden[6].ToUpper()}        {orden[7].ToUpper()}");
-                    Console.WriteLine($"--------------------------------------------------------");
+                if (fecha.Equals(string.Empty) || fecha.Equals(orden[2])) 
+                { 
+                PrintRow(orden[0].ToUpper(), orden[1].ToUpper(), orden[2].ToUpper(), orden[3].ToUpper(), orden[4].ToUpper(), orden[5].ToUpper(), orden[6].ToUpper(), orden[7].ToUpper());
+                PrintLine();
+                }
 
             }
 
-            Console.WriteLine($" ************************************************ ");
+            
             lector.Close();
             Console.ReadKey();
+        }
+
+        static void PrintLine()
+        {
+            Console.WriteLine(new string('-', tableWidth));
+        }
+
+        static void PrintRow(params string[] columns)
+        {
+            int width = (tableWidth - columns.Length) / columns.Length;
+            string row = "|";
+
+            foreach (string column in columns)
+            {
+                row += AlignCentre(column, width) + "|";
+            }
+
+            Console.WriteLine(row);
+        }
+
+        static string AlignCentre(string text, int width)
+        {
+            text = text.Length > width ? text.Substring(0, width - 3) + "..." : text;
+
+            if (string.IsNullOrEmpty(text))
+            {
+                return new string(' ', width);
+            }
+            else
+            {
+                return text.PadRight(width - (width - text.Length) / 2).PadLeft(width);
+            }
         }
 
     }
